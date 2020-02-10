6cd0710744e97eafffe3370e6995cc3fe2457470
Kaje <59009825+KajeA@users.noreply.github.com>
2020-02-10 14:10:31 -0700

    Add files via upload
    Added image files

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 // when the button is clicked RNG runs to pick one of three (one of three functions)
 
 
-
-
 // fetch arid planets     13 17
 
 function pickDesert() {
@@ -10,43 +8,109 @@ function pickDesert() {
    randomPlanet()
 
     function randomPlanet(){
+
         let planetSelection = Math.floor(Math.random() * Math.floor(3)); {
-          if (planetSelection === 0) {
-        url="https://swapi.co/api/planets/19/"   
+        if (planetSelection === 0) {
+             return"https://swapi.co/api/planets/19/"   
         } if (planetSelection === 1) {
-        url="https://swapi.co/api/planets/13/"     
+         return "https://swapi.co/api/planets/13/"     
         } if (planetSelection === 2) {
-      url="https://swapi.co/api/planets/17/"    
+             return "https://swapi.co/api/planets/17/"    
         }  
        
         } 
-        } 
+    } 
 
-
-
-fetch(url)  // HOW DO I MAKE URL GLOBAL??
-    .then(x => x.json())
-    .then(data => {
-        planetClimate(data.climate)
-        planetName(data.name)
-    })
-
-    function planetClimate(text) {
-        document.getElementById("climate").innerHTML = text
-  
-    }
-    function planetName(text) {
-        document.getElementById("planet").innerHTML = text
-
-    }
-
+    let url = randomPlanet()
+    getData(url)
 }
 
+ // fetch temperate planets  
+function pickTemperate() {
 
-
-// fetch temperate planets  3 7 8
+    randomPlanet()
+ 
+     function randomPlanet(){
+ 
+         let planetSelection = Math.floor(Math.random() * Math.floor(3)); {
+         if (planetSelection === 0) {
+              return"https://swapi.co/api/planets/3/"   
+         } if (planetSelection === 1) {
+          return "https://swapi.co/api/planets/7/"     
+         } if (planetSelection === 2) {
+              return "https://swapi.co/api/planets/8/"    
+         }  
+        
+         } 
+     } 
+ 
+     let url = randomPlanet()
+     getData(url)
+ }
+ 
 
 
 // fetch cold planets
 
-//16 4 55
+
+ function pickCold() {
+
+    randomPlanet()
+ 
+     function randomPlanet(){
+ 
+         let planetSelection = Math.floor(Math.random() * Math.floor(3)); {
+         if (planetSelection === 0) {
+              return"https://swapi.co/api/planets/16/"   
+         } if (planetSelection === 1) {
+          return "https://swapi.co/api/planets/4/"     
+         } if (planetSelection === 2) {
+              return "https://swapi.co/api/planets/55/"    
+         }  
+        
+         } 
+     } 
+ 
+     let url = randomPlanet()
+     getData(url)
+ }
+ 
+
+
+
+
+
+function getData(url) {
+
+    loading()
+    
+    fetch(url)  
+        .then(x => x.json())
+        .then(data => {
+            removeLoading()
+            planetClimate(data.climate)
+            planetName(data.name)
+        })
+
+        function planetClimate(text) {
+            document.getElementById("climate").innerHTML = text
+
+        }
+        function planetName(text) {
+            document.getElementById("planet").innerHTML = text
+
+        }
+    
+
+}
+
+function loading() {
+    document.getElementById("loadingStatus").innerHTML = "Loading... Punch it Chewy!"
+}
+
+function removeLoading() {
+    document.getElementById("loadingStatus").innerHTML = "Waiting for a selection..."
+}
+
+   
+
